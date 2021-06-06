39090bd8c6bfb29a52a4480265c1c0a33bb05d8e
KahefQazi <85427302+KahefQazi@users.noreply.github.com>
2021-06-06 22:07:33 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>India</h1>
+    </header>
+  ); 
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>N.K.Orchid College of Engineering and Technology ,Solapur</h1>
+      <p>
+        Our college has a well-developed infrastructure , pleasant
+        campus,brilliant and hardworking students most importantly very hard
+        working , devoted teaching staff , caring non-teaching staff. Our
+        principal sir laways tries his level best to provide us with all the
+        best sources for our improvement in our academics and all co-curricular
+        activties etc. We all are very much lucky to be a part of Orchid family.
+      </p>
+    </div>
+  );
+}
+export default Note;
